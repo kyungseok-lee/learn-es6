8b32f3b3d4e5505bddff26d54857dbd0cd228e83
kslee <meant0415@gmail.com>
2021-01-25 02:38:16 +0900

    init

---
+++ src/03_arrow/arrow-41.js
@@ -0,0 +1,14 @@
+"use strict";
+debugger;
+
+let sports = () => {
+	try {
+		let args = arguments; // ReferenceError: args is not defined, arrow function에서는 사용 불가
+	} catch (error) {
+		console.log("사용 불가");
+	}
+}
+sports(1, 2);
+
+
+let dummy;
